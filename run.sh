ec027403bc87e2e74a72651137c4fae394b8f7d9
chumanfu <christopher.mitchell.79@gmail.com>
2017-05-31 10:07:31 +0100

    Updated from PHP version 5.5

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ git clone git@github.com:chumanfu/chumanfu.github.io.git deploy/
 cp output/$TRAVIS_PHP_VERSION/* deploy/$TRAVIS_PHP_VERSION/
 git add --all
 git commit -am "Updated from PHP version $TRAVIS_PHP_VERSION"
-rm -Rf deploy
+# rm -Rf deploy
