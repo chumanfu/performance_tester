4c55e1d3da848a3fabfe00dd7d38f23dc3650068
chumanfu <christopher.mitchell.79@gmail.com>
2017-05-31 10:05:49 +0100

    Updated from PHP version

--- run.sh
+++ run.sh
@@ -2,3 +2,8 @@
 
 php src/test.php
 
+git clone git@github.com:chumanfu/chumanfu.github.io.git deploy/
+cp output/$TRAVIS_PHP_VERSION/* deploy/$TRAVIS_PHP_VERSION/
+git add --all
+git commit -am "Updated from PHP version $TRAVIS_PHP_VERSION"
+rm -Rf deploy
---
+++ run_local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+set TRAVIS_PHP_VERSION=$1
+./run.sh
