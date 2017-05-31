2ed26047b2aeffc90bcfdef63a8a7981e7d9f581
chumanfu <christopher.mitchell.79@gmail.com>
2017-05-31 10:14:42 +0100

    New build file

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ php src/test.php
 
 git clone git@github.com:chumanfu/chumanfu.github.io.git deploy/
 cp output/$TRAVIS_PHP_VERSION/* deploy/$TRAVIS_PHP_VERSION/
+cd deploy
 git add --all
 git commit -am "Updated from PHP version $TRAVIS_PHP_VERSION"
 git push
+cd ..
 rm -Rf deploy
